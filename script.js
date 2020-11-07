2ae4f3f8a4650a9d97bdedc8ab948e397141d888
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2020-11-06 17:37:15 -0700

    another test for list items

--- script.js
+++ script.js
@@ -24,18 +24,19 @@ if (localStorage.getItem("searchHistory")) {
 
   // searchHistory.push(searchHistoryEl);
 
-  // for (var i = 0; i < searchHistory.length; i++) {
+  for (var i = 0; i < searchHistory.length; i++) {
     var plantSearchHist = document.getElementById("saved-searches");
     console.log(plantSearchHist);
     var plantSearchHistListItem = document.createElement("li");
     plantSearchHistListItem.classList.add("collection-item")
 
     console.log(plantSearchHistListItem);
-    plantSearchHistListItem.append(searchHistory[i]);
+    plantSearchHistListItem.innerHTML(searchHistory[i])
+    // plantSearchHistListItem.append(plantSearchHist);
     plantSearchHist.append(plantSearchHistListItem);
 
     // searchHistory.push(plantSearchHistListItem);
-  // }
+  }
 
   // var test = document.getElementById("show-yourself");
   // console.log(test);
